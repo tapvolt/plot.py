0498994e45937d2afae0a23e4bbec9bef4986cb3
Gareth Jackson <gareth.jackson@gmail.com>
2023-12-31 22:02:21 +0000

    tidy up

--- plot.py
+++ plot.py
@@ -2,6 +2,7 @@ import argparse
 import serial
 import sys
 
+# Credit to Dhananjay Balan https://blog.dbalan.in/blog/2019/02/23/resurracting-an-hp-7440a-plotter/index.html
 
 def read_file(file):
     f = open(file, "r")
@@ -29,31 +30,27 @@ def stitch(hpgl_string, buffer_length=1024):
 def exec_hpgl(hpgl_string, port):
     # remove the last IN; that vpype always adds and breaks my plotter
     body = stitch(hpgl_string.rpartition("IN;")[0])
-    with serial.Serial(port, 9600, timeout=3) as plt:
+    with serial.Serial(port, 9600, timeout=5) as plt:
         for ins in body:
-            # size (Esc-B) returns bufferlen
-            plt.write((ins + "OA;").encode())
             # For block sent, end with OA, which reports back current position on the pen
+            plt.write((ins + "OA;").encode())
             c = bytearray()
             data = bytearray()
             while c.decode() != '\r':
                 c = plt.read()
                 data += c
-                # print("read: {}".format(map(ord, c.decode())))
             print("OA returns x, y, pen: {}".format(data.decode()))
             # We got data, mean OA got executed, so the instruction buffer is all consumed, ready to sent more.
 
 
 def parse():
     parser = argparse.ArgumentParser(description="Plot a file via a serial port.")
-
     parser.add_argument("--file", type=str, metavar="image.hpgl", help="A path to or the HPGL file you would like to plot.")
     parser.add_argument("--port", type=str, metavar="/dev/tty.usbserial-1220", help="The serial port to use.")
 
     if len(sys.argv) == 1:
         parser.print_help(sys.stderr)
         sys.exit(1)
-
     args = parser.parse_args()
     return args
 
